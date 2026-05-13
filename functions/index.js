8652adcf9d128e9f65dff27d923347b7a332cd4b
abenez3r1-create <abenez3r1@gmail.com>
2026-05-13 13:35:22 +0300

    setup for backend

---
+++ functions/index.js
@@ -0,0 +1,10 @@
+const { setGlobalOptions } = require("firebase-functions");
+const { onRequest } = require("firebase-functions/https");
+const logger = require("firebase-functions/logger");
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+const stripe = require("stripe")(process.env.STRIPE_KEY);
+
+setGlobalOptions({ maxInstances: 10 });
--- src/Utility/firebase.js
+++ src/Utility/firebase.js
@@ -1,19 +1,16 @@
-import firebase from "firebase/compat/app";
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import "firebase/compat/firestore";
-import "firebase/compat/auth";
+import { getFirestore } from "firebase/firestore";
+
 const firebaseConfig = {
   apiKey: "AIzaSyD4lJrUNMQfuS12QyY44CoexoZV3hfRPJk",
   authDomain: "clone-47671.firebaseapp.com",
   projectId: "clone-47671",
-  storageBucket: "clone-47671.firebasestorage.app",
+  storageBucket: "clone-47671.appspot.com",
   messagingSenderId: "736945052312",
   appId: "1:736945052312:web:692919f9a758e1af024eed",
 };
 
-// Initialize Firebase
-
-const app = firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = app.firestore;
+export const db = getFirestore(app);
